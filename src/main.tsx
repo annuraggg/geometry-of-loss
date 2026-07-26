a5923a87c4ffdee99ce8b9437a95a666c30b8c2e
annuragggg <anuragsawant@duck.com>
2026-07-26 23:14:31 +0530

    Refactor project configuration and add @types/node dependency

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,11 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./styles.css";
 
-createRoot(document.getElementById("root")!).render(
+const basename = import.meta.env.VITE_BASE_PATH || "/";
+
+  createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
